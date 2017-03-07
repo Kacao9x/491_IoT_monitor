4ec609bfae084dd3206bbb731028861ba5399e4f
Kacao Cao <kacao@iastate.edu>
2017-03-07 10:25:45 -0600

    add comment for sample code

--- FONAtest/FONAtest.ino
+++ FONAtest/FONAtest.ino
@@ -54,43 +54,43 @@ uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 uint8_t type;
 
 void setup() {
-  while (!Serial);
-
-  Serial.begin(115200);
-  Serial.println(F("FONA basic test"));
-  Serial.println(F("Initializing....(May take 3 seconds)"));
-
-  fonaSerial->begin(4800);
-  if (! fona.begin(*fonaSerial)) {
-    Serial.println(F("Couldn't find FONA"));
-    while (1);
-  }
-  type = fona.type();
-  Serial.println(F("FONA is OK"));
-  Serial.print(F("Found "));
-  switch (type) {
-    case FONA800L:
-      Serial.println(F("FONA 800L")); break;
-    case FONA800H:
-      Serial.println(F("FONA 800H")); break;
-    case FONA808_V1:
-      Serial.println(F("FONA 808 (v1)")); break;
-    case FONA808_V2:
-      Serial.println(F("FONA 808 (v2)")); break;
-    case FONA3G_A:
-      Serial.println(F("FONA 3G (American)")); break;
-    case FONA3G_E:
-      Serial.println(F("FONA 3G (European)")); break;
-    default: 
-      Serial.println(F("???")); break;
-  }
+        while (!Serial);
+        
+          Serial.begin(115200);
+          Serial.println(F("FONA basic test"));
+          Serial.println(F("Initializing....(May take 3 seconds)"));
+        
+          fonaSerial->begin(4800);
+          if (! fona.begin(*fonaSerial)) {
+                Serial.println(F("Couldn't find FONA"));
+                while (1);
+         }
+          type = fona.type();
+          Serial.println(F("FONA is OK"));
+          Serial.print(F("Found "));
+          switch (type) {
+                    case FONA800L:
+                        Serial.println(F("FONA 800L")); break;
+                    case FONA800H:
+                        Serial.println(F("FONA 800H")); break;
+                    case FONA808_V1:
+                        Serial.println(F("FONA 808 (v1)")); break;
+                    case FONA808_V2:
+                        Serial.println(F("FONA 808 (v2)")); break;
+                    case FONA3G_A:
+                        Serial.println(F("FONA 3G (American)")); break;
+                    case FONA3G_E:
+                        Serial.println(F("FONA 3G (European)")); break;
+                    default: 
+                        Serial.println(F("???")); break;
+         }
   
-  // Print module IMEI number.
-  char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
-  uint8_t imeiLen = fona.getIMEI(imei);
-  if (imeiLen > 0) {
-    Serial.print("Module IMEI: "); Serial.println(imei);
-  }
+          // Print module IMEI number.
+          char imei[15] = {0}; // MUST use a 16 character buffer for IMEI!
+          uint8_t imeiLen = fona.getIMEI(imei);
+          if (imeiLen > 0) {
+                Serial.print("Module IMEI: "); Serial.println(imei);
+          }
 
   // Optionally configure a GPRS APN, username, and password.
   // You might need to do this to access your network's GPRS/data
@@ -104,91 +104,93 @@ void setup() {
   // the following line then redirects over SSL will be followed.
   //fona.setHTTPSRedirect(true);
 
-  printMenu();
+        printMenu();
 }
 
+//Display the menu
 void printMenu(void) {
-  Serial.println(F("-------------------------------------"));
-  Serial.println(F("[?] Print this menu"));
-  Serial.println(F("[a] read the ADC 2.8V max (FONA800 & 808)"));
-  Serial.println(F("[b] read the Battery V and % charged"));
-  Serial.println(F("[C] read the SIM CCID"));
-  Serial.println(F("[U] Unlock SIM with PIN code"));
-  Serial.println(F("[i] read RSSI"));
-  Serial.println(F("[n] get Network status"));
-  Serial.println(F("[v] set audio Volume"));
-  Serial.println(F("[V] get Volume"));
-  Serial.println(F("[H] set Headphone audio (FONA800 & 808)"));
-  Serial.println(F("[e] set External audio (FONA800 & 808)"));
-  Serial.println(F("[T] play audio Tone"));
-  Serial.println(F("[P] PWM/Buzzer out (FONA800 & 808)"));
+          Serial.println(F("-------------------------------------"));
+          Serial.println(F("[?] Print this menu"));
+          Serial.println(F("[a] read the ADC 2.8V max (FONA800 & 808)"));
+          Serial.println(F("[b] read the Battery V and % charged"));
+          Serial.println(F("[C] read the SIM CCID"));
+          Serial.println(F("[U] Unlock SIM with PIN code"));
+          Serial.println(F("[i] read RSSI"));
+          Serial.println(F("[n] get Network status"));
+          Serial.println(F("[v] set audio Volume"));
+          Serial.println(F("[V] get Volume"));
+          Serial.println(F("[H] set Headphone audio (FONA800 & 808)"));
+          Serial.println(F("[e] set External audio (FONA800 & 808)"));
+          Serial.println(F("[T] play audio Tone"));
+          Serial.println(F("[P] PWM/Buzzer out (FONA800 & 808)"));
 
-  // FM (SIM800 only!)
-  Serial.println(F("[f] tune FM radio (FONA800)"));
-  Serial.println(F("[F] turn off FM (FONA800)"));
-  Serial.println(F("[m] set FM volume (FONA800)"));
-  Serial.println(F("[M] get FM volume (FONA800)"));
-  Serial.println(F("[q] get FM station signal level (FONA800)"));
+          // FM (SIM800 only!)
+          Serial.println(F("[f] tune FM radio (FONA800)"));
+          Serial.println(F("[F] turn off FM (FONA800)"));
+          Serial.println(F("[m] set FM volume (FONA800)"));
+          Serial.println(F("[M] get FM volume (FONA800)"));
+          Serial.println(F("[q] get FM station signal level (FONA800)"));
 
-  // Phone
-  Serial.println(F("[c] make phone Call"));
-  Serial.println(F("[A] get call status"));
-  Serial.println(F("[h] Hang up phone"));
-  Serial.println(F("[p] Pick up phone"));
+          // Phone
+          Serial.println(F("[c] make phone Call"));
+          Serial.println(F("[A] get call status"));
+          Serial.println(F("[h] Hang up phone"));
+          Serial.println(F("[p] Pick up phone"));
 
-  // SMS
-  Serial.println(F("[N] Number of SMSs"));
-  Serial.println(F("[r] Read SMS #"));
-  Serial.println(F("[R] Read All SMS"));
-  Serial.println(F("[d] Delete SMS #"));
-  Serial.println(F("[s] Send SMS"));
-  Serial.println(F("[u] Send USSD"));
+          // SMS
+          Serial.println(F("[N] Number of SMSs"));
+          Serial.println(F("[r] Read SMS #"));
+          Serial.println(F("[R] Read All SMS"));
+          Serial.println(F("[d] Delete SMS #"));
+          Serial.println(F("[s] Send SMS"));
+          Serial.println(F("[u] Send USSD"));
   
-  // Time
-  Serial.println(F("[y] Enable network time sync (FONA 800 & 808)"));
-  Serial.println(F("[Y] Enable NTP time sync (GPRS FONA 800 & 808)"));
-  Serial.println(F("[t] Get network time"));
+          // Time
+          Serial.println(F("[y] Enable network time sync (FONA 800 & 808)"));
+          Serial.println(F("[Y] Enable NTP time sync (GPRS FONA 800 & 808)"));
+          Serial.println(F("[t] Get network time"));
 
-  // GPRS
-  Serial.println(F("[G] Enable GPRS"));
-  Serial.println(F("[g] Disable GPRS"));
-  Serial.println(F("[l] Query GSMLOC (GPRS)"));
-  Serial.println(F("[w] Read webpage (GPRS)"));
-  Serial.println(F("[W] Post to website (GPRS)"));
+          // GPRS
+          Serial.println(F("[G] Enable GPRS"));
+          Serial.println(F("[g] Disable GPRS"));
+          Serial.println(F("[l] Query GSMLOC (GPRS)"));
+          Serial.println(F("[w] Read webpage (GPRS)"));
+          Serial.println(F("[W] Post to website (GPRS)"));
 
-  // GPS
-  if ((type == FONA3G_A) || (type == FONA3G_E) || (type == FONA808_V1) || (type == FONA808_V2)) {
-    Serial.println(F("[O] Turn GPS on (FONA 808 & 3G)"));
-    Serial.println(F("[o] Turn GPS off (FONA 808 & 3G)"));
-    Serial.println(F("[L] Query GPS location (FONA 808 & 3G)"));
-    if (type == FONA808_V1) {
-      Serial.println(F("[x] GPS fix status (FONA808 v1 only)"));
-    }
-    Serial.println(F("[E] Raw NMEA out (FONA808)"));
-  }
+          // GPS
+          if ((type == FONA3G_A) || (type == FONA3G_E) || (type == FONA808_V1) || (type == FONA808_V2)) {
+                Serial.println(F("[O] Turn GPS on (FONA 808 & 3G)"));
+                Serial.println(F("[o] Turn GPS off (FONA 808 & 3G)"));
+                Serial.println(F("[L] Query GPS location (FONA 808 & 3G)"));
+                if (type == FONA808_V1) {
+                        Serial.println(F("[x] GPS fix status (FONA808 v1 only)"));
+                }
+                Serial.println(F("[E] Raw NMEA out (FONA808)"));
+          }
   
-  Serial.println(F("[S] create Serial passthru tunnel"));
-  Serial.println(F("-------------------------------------"));
-  Serial.println(F(""));
+          Serial.println(F("[S] create Serial passthru tunnel"));
+          Serial.println(F("-------------------------------------"));
+          Serial.println(F(""));
 
 }
 void loop() {
-  Serial.print(F("FONA> "));
-  while (! Serial.available() ) {
-    if (fona.available()) {
-      Serial.write(fona.read());
-    }
-  }
+          Serial.print(F("FONA> "));
+          /* print out the input character if while loop misses checking signal*/
+          while (! Serial.available() ) {
+                if (fona.available()) {
+                        Serial.write(fona.read());
+                 }
+         }
 
-  char command = Serial.read();
-  Serial.println(command);
+          char command = Serial.read();
+          Serial.println(command);
 
 
-  switch (command) {
-    case '?': {
-        printMenu();
-        break;
-      }
+         switch (command) {
+                case '?': {
+                        printMenu();
+                        break;
+                 }
 
     case 'a': {
         // read the ADC
