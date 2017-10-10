805f99fd9a23e4f71f00711c60bcd93c00a743bf
Kacao Cao <kacao@iastate.edu>
2017-10-09 23:19:46 -0500

    add comment to explain function of the program

--- RF_receive/RF_receive.ino
+++ RF_receive/RF_receive.ino
@@ -1,7 +1,14 @@
 #include <SPI.h>  
 #include "RF24.h" 
 
+/* set up communication pipe:
+Address: 0 | channel: 115 | Max_power | data rate: 250kb (lowest - better range)
+
+Speed options: RF24_250KBPS | RF24_1MBPS | RF24_2MBPS
+Power consump: PA_MIN | PA_LOW | PA_HIGH | PA_MAX
+*/
 RF24 myRadio (7, 8); 
+/* create a data structure to store the data sent from transmitter */
 struct package
 {
   int id=0;
@@ -10,8 +17,6 @@ struct package
 };
 
 byte addresses[][6] = {"0"}; 
-
-
 
 typedef struct package Package;
 Package data;
@@ -22,22 +27,22 @@ void setup()
   delay(1000);
 
   myRadio.begin(); 
-  myRadio.setChannel(115); 
-  myRadio.setPALevel(RF24_PA_MAX);
-  myRadio.setDataRate( RF24_250KBPS ) ; 
-  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.begin();  
+  myRadio.setChannel(115);                  //Communication channel: 115 
+  myRadio.setPALevel(RF24_PA_MAX);          //Max power to enhance comm range
+  myRadio.setDataRate( RF24_250KBPS ) ;     //250kB lowest data transfer rate --> achieve better range
+  myRadio.openReadingPipe(1, addresses[0]); //open pipe to echo the data streaming to
   myRadio.startListening();
 }
 
 
 void loop()  
 {
-
-  if ( myRadio.available()) 
+  if ( myRadio.available())                 // if RF on
   {
-    while (myRadio.available())
+    while (myRadio.available())             // if RF still on
     {
-      myRadio.read( &data, sizeof(data) );
+      myRadio.read( &data, sizeof(data) );  //populate data into the Data structure
     }
     Serial.print("\nPackage:");
     Serial.print(data.id);
--- RF_send/RF_send.ino
+++ RF_send/RF_send.ino
@@ -1,14 +1,21 @@
 #include <SPI.h>  
 #include "RF24.h"
 
+/* set up communication pipe:
+Address: 0 | channel: 115 | Max_power | data rate: 250kb (lowest - better range)
+
+Speed options: RF24_250KBPS | RF24_1MBPS | RF24_2MBPS
+Power consump: PA_MIN | PA_LOW | PA_HIGH | PA_MAX
+*/
 RF24 myRadio (7, 8);
 byte addresses[][6] = {"0"};
 
+/* create a data structure to store the data that needed to transmit */
 struct package
 {
   int id=1;
   float temperature = 18.3;
-  char  text[100] = "Text to be transmitted";
+  char  text[100] = "Temperature value here";
 };
 
 typedef struct package Package;
@@ -16,19 +23,19 @@ Package data;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200);                     //Set serial speed
   delay(1000);
   myRadio.begin();  
-  myRadio.setChannel(115); 
-  myRadio.setPALevel(RF24_PA_MAX);
-  myRadio.setDataRate( RF24_250KBPS ) ; 
-  myRadio.openWritingPipe( addresses[0]);
+  myRadio.setChannel(115);                  //Communication channel: 115 
+  myRadio.setPALevel(RF24_PA_MAX);          //Max power to enhance comm range
+  myRadio.setDataRate( RF24_250KBPS ) ;     //250kB lowest data transfer rate --> achieve better range
+  myRadio.openWritingPipe( addresses[0]);   //open pipe to send data
   delay(1000);
 }
 
 void loop()
 {
-  myRadio.write(&data, sizeof(data)); 
+  myRadio.write(&data, sizeof(data));       //populate the data structure
 
   Serial.print("\nPackage:");
   Serial.print(data.id);
@@ -39,4 +46,5 @@ void loop()
   data.temperature = data.temperature+0.1;
   delay(1000);
 
+/* CALCULATE how long to transmit data package*/
 }
