2b40c95ace7bef09dfe9386664cc0a330f23e891
Kacao Cao <kacao@iastate.edu>
2017-11-30 14:17:24 -0600

    (1) adding RF transceiver operation + passed compiling error (2) add comment for the loop archietecture, need to implement

--- Home-Node-redesign/Home-Node-redesign.ino
+++ Home-Node-redesign/Home-Node-redesign.ino
@@ -46,16 +46,13 @@ bool old_Data=true;
 
 void setup() {
   /* Set up 2G network here*/
-  client_2G.begin(19200);                  //the GPRS baud rate
-  Serial.begin(19200);                    // Serial monitor baud rate
+  client_2G.begin(19200);                   //the GPRS baud rate
+  Serial.begin(19200);                      // Serial monitor baud rate
   delay(500);
 
   /* Set up RF radio reception here*/
-  for( i=0; i<Max_Nodes; i++){
-      My_Data.path[i] = 0;
-    }
-     My_Data.return_flag=0;
-     My_Data.Place_In_Path=1;
+    _clear_data_struct();
+     My_Data.Place_In_Path=1;               //CHANNGEE ----------------------------------??
 
     My_Data.sensor1 = NodeData;
     Serial.begin(9600);
@@ -86,8 +83,31 @@ void loop() {
       delay(1000);
       Serial.println(" POST something ");
 
-      //Submit_HTTP_request();
-      /* if null path, exit the condition*/
+      /**
+       * GET: checking a new path from website 
+       * if the path != 0 or flag == 1 execute performance
+       * else return
+       * 
+       *    ...<if> read_path, store in My_Data.path[]
+       *    Integrate with T&S tranceiver
+       *      Zero_out the path [0,0,0,0]
+       *    POST Received.data + flag == done?
+       *    
+       *    return
+       *    
+       */
+      
+      //zero_out the path
+      /*
+      Submit_HTTP_request();
+      _CastString_to_Int_Array();
+      if (web_flag == 0) return;
+      else {
+        //Tim+Steve transmission
+        //zero_out path
+        Post_Http_request();
+      } */
+      
       
     }
   
@@ -100,10 +120,10 @@ void loop() {
 
 void ShowSerialData() {
 
-  int8_t i = 0;
+  int8_t w = 0;
   while( client_2G.available() != 0)
     Serial.write( client_2G.read() );
-    reading[i] = client_2G.read();
+    reading[w] = client_2G.read();
     delay(100);
     i++;
     
@@ -212,4 +232,124 @@ void Post_Http_request()
     ShowSerialData();
 }
 
+void _CastString_to_Int_Array() {
+
+  return;
+}
+void serial_logger(){
+
+    //clear path
+    _clear_data_struct();
+     My_Data.Place_In_Path=1;         //CHANGE ME ---------------------??
+
+    //reading[] = {5,3,2,4,1}; populate My_data.path[] by the array
+    My_Data.path[0] = 0;
+    for( int8_t k = 0; k< sizeof(reading); k++) {
+      My_Data.path[k+1] = int( reading[k] );
+    }
+    
+    //CHANGE ME
+//    int c = int(Serial.read());
+//    if ( c == '1'){ 
+//       //path to 1
+//        My_Data.path[0] = 0;
+//        My_Data.path[1] = 1;     
+//      Serial.println(F("Calling node 1"));
+//   }
+//
+//    else if ( c == '2'){ 
+//        //path to 2
+//        My_Data.path[0] = 0;
+//        My_Data.path[1] = 1;
+//        My_Data.path[2] = 2;   
+//      Serial.println(F("Calling node 2"));
+//   }
+//     else{
+//    return;
+//   }
+
+    Serial.print("print mydata struct: ");
+   for( int8_t w = 0; w< sizeof(reading); w++) {
+      Serial.print(My_Data.path[w]);
+    }
+    Serial.println(" ");
+    Serial.println("-------------");
+  
+   transmit(My_Data);
+    //RECIEVE
+    delay(20);
+    start_time=millis();
+     Serial.println("---Listening For Response---");
+     while(start_time+Timeout>millis()){
+      receive();
+     }
+  Serial.println("restart loop");
+
+}
+
+
+void receive(){                                                             //Recieve Data from another node
+    radio.openReadingPipe(1,addresses[0]);
+    radio.startListening();  
+       if(radio.available()){  
+            radio.read(&Received_Data, sizeof(MsgData));  //byte value
+          /*
+            Serial.println("\nRecieved Data");
+           
+           Serial.print("ID: ");
+           Serial.println(Received_Data.ID);
+           Serial.print("Place_In_Path: ");
+           Serial.println(Received_Data.Place_In_Path);
+           Serial.print("Path: ");
+           for (int i=0;i<Max_Nodes;i++){
+             Serial.print(Received_Data.path[i]);
+             Serial.print(", ");
+           }
+           Serial.println("");
+           Serial.print("Return_Flag: ");
+           Serial.println(Received_Data.return_flag);
+            delay(5);
+          */
+      }
+    return;
+}
+
+void transmit(MsgData Transmit_Msg){                                        //Transmit Data to Another Node
+    radio.openWritingPipe(addresses[0]);
+    radio.stopListening();
+    
+  for(i=0; i<TransAMOUNT; i++){  
+        radio.write(&Transmit_Msg, sizeof(MsgData));
+        delay(5);
+  }
+                   Serial.println("\nTransmitted Data");
+           
+           Serial.print("ID: ");
+           Serial.println(Transmit_Msg.ID);
+
+           Serial.print("Place_In_Path: ");
+           Serial.println(Transmit_Msg.Place_In_Path);
+
+           Serial.print("Path: ");
+           for (i=0;i<Max_Nodes;i++){
+             Serial.print(Transmit_Msg.path[i]);
+             Serial.print(", ");
+           }
+           Serial.println("");
+
+           Serial.print("Return_Flag: ");
+           Serial.println(Transmit_Msg.return_flag);
+        delay(5);
+        Serial.print("Path: ");
+}
+
+/**
+ * Helper method to zero out the Msg_Data struct
+ */
+void _clear_data_struct() {
+    for( i=0; i<Max_Nodes; i++){
+      My_Data.path[i] = 0;
+    }
+     My_Data.return_flag=0;
+}
 
