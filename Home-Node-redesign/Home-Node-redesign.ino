e612019993cee29a4f82f454b4c1449356f1ab50
Kacao Cao <kacao@iastate.edu>
2017-12-07 00:19:18 -0500

    code for prototype: <passed> transceiver <passed> request http, read path <in progress> Post http <passed> sleep mode

--- Home-Node-redesign/Home-Node-redesign.ino
+++ Home-Node-redesign/Home-Node-redesign.ino
@@ -6,20 +6,21 @@
 /* data structure for the tranmission */
 //unsigned long msg;
 const byte NodeID = 0;                      //Node ID for home station
-float NodeData = 2;                   //Home_node.NodeData = time sleep for low power mode.
+float NodeData = 3;                         //Home_node.NodeData = time sleep for low power mode.
 const int Max_Nodes = 20;
-byte reading_temp[Max_Nodes];
-byte reading[Max_Nodes];               //change to dynamic array then?
+//byte reading_temp[Max_Nodes];
+//byte reading[Max_Nodes];                    //change to dynamic array then?
 
-byte readingHold[46];   //this would hold the response of the AT+HTTPREAD
-byte readingHold2[11];  //This contains only the data which is the path e.g[5,4,3,2,1]
+byte readingHold[46];                       //this would hold the response of the AT+HTTPREAD
+byte readingHold2[Max_Nodes];               //This contains only the data which is the path e.g[5,4,3,2,1]
+byte readingHold3[Max_Nodes];               //This contains the final path to copy to My_Data.path[ ]    
 
 /* Variables for 2G connection */
-SoftwareSerial client_2G(7, 8);            //2G network pin 7:Rx (CE), pin 8(CSN): Tx, pin 9: power Up/Down for SW reset
+SoftwareSerial client_2G(7, 8);             //2G network pin 7:Rx (CE), pin 8(CSN): Tx, pin 9: power Up/Down for SW reset
 
 /* Variable for RF communication */
 RF24 radio(2, 3);
-const uint64_t pipe = 0xE8E8F0F0E1LL; //channel to recieve
+const uint64_t pipe = 0xE8E8F0F0E1LL;       //channel to recieve
 byte addresses[][6] = {"1Node", "2Node"};
 
 typedef struct {
@@ -28,7 +29,7 @@ typedef struct {
   byte Place_In_Path;                       //Where in the array are we
   byte cmd;                                 //go to sleep, other odd commands
   bool return_flag;                         //Return to home node, go from ++ to --
-  float sensor1;                            //set time for sleep (shared by HOME + OPNODE)
+  float sensor1;                            //sleep time(sent from HomeNode) or sensor value(sent by Op-Node)
 } MsgData;
 
 MsgData My_Data;
@@ -57,22 +58,20 @@ void setup() {
   delay(500);
 
   /* Set up RF radio reception here*/
-  _clear_data_struct();
-  My_Data.Place_In_Path = 1;             //CHANNGEE ----------------------------------??
-
-  My_Data.sensor1 = NodeData;             //sleep time for low-power mode. Sent out to Op-Node to make sure compatibility
   Serial.begin(9600);
-  radio.begin();
-  radio.setAutoAck(false);
-  radio.openReadingPipe(1, pipe);
-  radio.startListening();
+    radio.begin();
+    radio.setAutoAck(false);
+    radio.openReadingPipe(1, pipe);
+    radio.startListening();
+
+  /* init variable */
+    _clear_data_struct();
+    My_Data.sensor1 = NodeData;             //sleep time for low-power mode. Sent out to Op-Node to make sure compatibility
 
   /* Run once to go end-to-end */
-  //connect_GPRS();
+  connect_GPRS();
   //Submit_HTTP_request();
-  if (client_2G.println("AT+CSQ"))
-    Serial.print("Connected_setup");
-
+  Serial.println(" \n\n--------- Setup complete ---------- \n\n");
 }
 
 void loop() {
@@ -87,20 +86,20 @@ void loop() {
       Serial.println(" POST something ");
 
       connect_GPRS();
-      memset(reading, 0, sizeof(reading));
-      Submit_HTTP_request();
+
+      /* Read the path from website */
+      Serial.println("read the website");
+      Submit_HTTP_request();                                    //Read path from the website
 
       //Debugging 
-      Serial.println("read the website");
       Serial.println("MAIN LOOP_check it out the path");
-      
-      for (i = 0; i < sizeof(reading); i++) {
-        Serial.print(reading[i]); Serial.print(" : ");
-        delay(100);
-      }
-      Serial.println(" ");
 
-
+      /* RF transceier enabled to parse the path */
+      serial_logger();
+      //value = Received_Data.sensor1;
+        if( (Received_Data.path[Received_Data.Place_In_Path] == 0) ) {
+            Serial.print("Data received from Serial log");Serial.print(Received_Data.ID);Serial.print(", ");Serial.println(Received_Data.sensor1);
+        }
       /* Need to check the condition, cannot exit when reading =0 
       if (reading[0] == 0 && reading[1] == 0) {
         Serial.println("what the fuck??? How come you get in here");
@@ -110,13 +109,13 @@ void loop() {
       else {
         serial_logger();
       } */
-
-      serial_logger();
+      
       /*POST data to website*/
       Serial.println("POST data to web");
       while ( POST_done_flag == 0 ) {
-        Post_HTTP_request();
-        Submit_HTTP_request();            // confirmation flag that data is sent successfully to web. ASK WEBTEAM to design a flag-return
+        Post_HTTP_request();              //relay the sensor datas to webserver
+        delay(1000);
+        Submit_HTTP_request();            // reading the confirm flag indicating that data is sent successfully to web. ASK WEBTEAM to design a flag-return
         POST_done_flag = 1;               // ---------------------- dummy value until Webteam get that done.
       }
 
@@ -128,31 +127,31 @@ void loop() {
 
       serial_logger();                    //passing sleep command to all Sensor node.
       delay(1000);
-      /* sleep mode */
       
+      /* sleep mode */ 
       Serial.println("Enter sleep mode zzzzzzzzzzzzzzzzz");
-      Serial.println("Disable the power off to save time demoing");
-      delay(500);
-//      power_cycle();
-//      if ( My_Data.cmd = 1 ) {
-//        for (i = 0; i < NodeData; i++) {
-//          LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
-//                        SPI_OFF, USART0_OFF, TWI_OFF);
-//        }
-//      }
+      power_cycle();                      //turn off 2G module
+      delay(10000);
+      if ( My_Data.cmd = 1 ) {
+        for (i = 0; i < NodeData; i++) {
+          LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                        SPI_OFF, USART0_OFF, TWI_OFF);
+        }
+        My_Data.cmd = 0;                    //zero out sleep cmd
+      }
 
       /* Sleep time is over, wake up*/
-      My_Data.cmd = 0;                    //zero out sleep cmd
-
+      //My_Data.cmd = 0;                    //zero out sleep cmd
     }
     return;
   } else {  //perform power_on
     //If no power at the beginning, works fine. But if mannually turn off, never back on???
-    Serial.println(" Loop_no_Connected - power reset");
-    power_cycle();
+    Serial.println( client_2G.available() );
+    Serial.println(" \nLoop_no_Connected - power reset");
+    power_cycle();                      //If module is off, turn ON
     delay(1000);
-    //connect_GPRS();
-    Serial.println("No need enable GPRS");                //Run once
+    //connect_GPRS();                     //No need GPRS setup here?
+    Serial.println("---- complete power cycle - go back to loop -----\n\n\n");                //Run once
   }
 }
 
@@ -173,20 +172,14 @@ void power_cycle() {
   delay(3000);
 }
 
-/*
-void power_off() {
-  client_2G.println("AT+HTTPTERM");        //terminate the HTTP, cause POWER RESET ----------------- CAUTION
-  delay(1000);
-  ShowSerialData();
-} */
 
 void connect_GPRS() {
   client_2G.println("AT+CSQ");                                 //Check signal quality
-  delay(100);
+  delay(500);
   //ShowSerialData();
 
   client_2G.println("AT+CGATT?");                             //Attach or Detach from GPRS Support
-  delay(100);
+  delay(500);
   //ShowSerialData();
 
   client_2G.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");     //setting the SAPBR, the connection type is using gprs
@@ -194,25 +187,38 @@ void connect_GPRS() {
   ShowSerialData();
 
   client_2G.println("AT+SAPBR=3,1,\"APN\",\"TRACFONE-WFM\""); //setting the APN, Access point name string
-  delay(4000);
+  delay(1000);
   //ShowSerialData();
 
   client_2G.println("AT+SAPBR=1,1");                          //setting the SAPBR
-  delay(2000);
+  delay(1000);
   //ShowSerialData();
 
   //  client_2G.println("AT+SAPBR=2,1");
   //    delay(1000);
   //    ShowSerialData();
 }
+void connect_HTTP() {
+  client_2G.println("AT+HTTPINIT");           //init the HTTP request
+    delay(2000);
+    ShowSerialData();
 
+  client_2G.println("AT+HTTPPARA=\"CID\",1");
+    delay(1000);
+    ShowSerialData();
+
+}
 void Submit_HTTP_request() {
   client_2G.println("AT+HTTPINIT");           //init the HTTP request
-  delay(2000);
+  delay(1000);
   ShowSerialData();
+//
+//  client_2G.println("AT+HTTPPARA=\"CID\",1");      //HTTP://sensorweb.ece.iastate.edu/api/4/path
+//  delay(1000);
+//  ShowSerialData();
 
   //this is a modification to obtain path from api
-  client_2G.println("AT+HTTPPARA=\"URL\",\"HTTP://sensorweb.ece.iastate.edu/api/4/path\"");//Public server IP address
+  client_2G.println("AT+HTTPPARA=\"URL\",\"http://sensorweb.ece.iastate.edu/api/2/path\"");//Public server IP address
   delay(1000);
   ShowSerialData();
 
@@ -225,15 +231,12 @@ void Submit_HTTP_request() {
   ShowSerialData();
 
   client_2G.println("AT+HTTPREAD");       //Send a command to read the HTTP server response, run AT+HTTPACTION in prior
-  delay(300);
-  //ShowSerialData();
+  delay(1000);
   Reading_Path();
-  //_convert_Str_to_IntArray(readingHold[2]);
 
   client_2G.println("");
   delay(100);
 }
-
 void Post_HTTP_request()
 {
   /* this function specifically extract only the path and not the other response retrieved from
@@ -242,10 +245,11 @@ void Post_HTTP_request()
 
   /* make a post to the server with sensor value */
   client_2G.println("AT+HTTPINIT");           //init the HTTP request
-  delay(2000);
+  delay(1000);
   ShowSerialData();
 
-  client_2G.println("AT+HTTPPARA=\"URL\",\"HTTP://sensorweb.ece.iastate.edu/api/homenode\"");//Public server IP address
+  //client_2G.println("AT+HTTPPARA=\"URL\",\"HTTP://sensorweb.ece.iastate.edu/api/homenode\"");//Public server IP address
+  client_2G.println("AT+HTTPPARA=\"URL\",\"HTTP://posttestserver.com/post.php?dir=Homenodetestiastate1\"");//Public server IP address
   delay(1000);
   ShowSerialData();
 
@@ -255,7 +259,7 @@ void Post_HTTP_request()
   ShowSerialData();
 
 
-  client_2G.println("AT+HTTPDATA=" + String(sizeof(reading)) + ",100000"); //insert data into the json format
+  client_2G.println("AT+HTTPDATA=" + String(sizeof(readingHold2)) + ",100000"); //insert data into the json format
   delay(1000);
   ShowSerialData();
 
@@ -263,10 +267,9 @@ void Post_HTTP_request()
   delay(1000);
   ShowSerialData();
 
-  /*
     client_2G.println("AT+HTTPTERM");        //terminate the HTTP, cause POWER RESET ----------------- CAUTION
       delay(1000);
-      ShowSerialData(); */
+      ShowSerialData();
 }
 
 void ShowSerialData() {
@@ -276,50 +279,30 @@ void ShowSerialData() {
   delay(100);
 }
 
-void _CastString_to_Int_Array() {
-
-  return;
-}
-
 
 void serial_logger() {
   Serial.println("enter serial_logger");
-  //clear path                      // ------------------------------------------------- Do we accidentally clear data path here?
+  //clear path
   _clear_data_struct();
-  My_Data.Place_In_Path = 1;       //CHANGE ME ---------------------??
 
-  //reading[] = {5,3,2,4,1}; populate My_data.path[] by the array
-//  for ( i = 0; i < sizeof(reading); i++) {
-//    My_Data.path[i] = int( reading[i] );
-//  }
+//-------------------- TEMP test
+//    reading_temp[0] = 0;
+//    reading_temp[1] = 1;
+//    reading_temp[2] = 2;
+//    reading_temp[3] = 0;
+//    for( i=0; i<Max_Nodes; i++) {
+//        My_Data.path[i] = reading[i];
+//    }
 
-  //CHANGE ME
-  //    int c = int(Serial.read());
-  //    if ( c == '1'){
-  //       //path to 1
-  //        My_Data.path[0] = 0;
-  //        My_Data.path[1] = 1;
-  //      Serial.println(F("Calling node 1"));
-  //   }
-
-  //     else{
-  //    return;
-  //   }
-
-    reading_temp[0] = 0;
-    reading_temp[1] = 1;
-    reading_temp[2] = 2;
-    reading_temp[3] = 0;
-    for( i=0; i<Max_Nodes; i++) {
-        My_Data.path[i] = reading_temp[i];
-    }
+    pathToSend();
+    
   Serial.print("Serial_logger_print mydata struct: ");
-  for ( int8_t i = 0; i < sizeof(reading_temp); i++) {
+  for ( int8_t i = 0; i < sizeof(My_Data.path); i++) {
     Serial.print(My_Data.path[i]);
   }
   delay(100);
-  Serial.print("print sleep cmd flag: "); Serial.println(My_Data.cmd);
-  Serial.println(" ");
+  Serial.print("\nprint sleep cmd flag: "); Serial.println(My_Data.cmd);
+  Serial.println("\n");
   Serial.println("-------------");
 
   transmit(My_Data);
@@ -403,6 +386,7 @@ void _clear_data_struct() {
     My_Data.path[i] = 0;
   }
   My_Data.return_flag = 0;
+  My_Data.Place_In_Path = 1;
 }
 
 
@@ -410,45 +394,141 @@ void _clear_data_struct() {
 
 /* --------------------------------------- TEMP ----------------------- */
 void Reading_Path() {
-  i = 0;
   Serial.println(" value from readingHold");
+
+  i = 0;
   while ( client_2G.available() ) {
     readingHold[i] = byte(client_2G.read());
-    Serial.print(readingHold[i]); Serial.print(":");
-    delay(100);
     i++;
   }
 
   /* Terver's idea: a temp solution for this problem. Not a generic to be used everywhere.*/
-  memset(readingHold2, 0, sizeof(readingHold2));
+  //memset(readingHold2, 0, sizeof(readingHold2));
   int m = 29;
   Serial.println(" ");
-  Serial.println(" debugging reading path ----------------------");
-  for (i = 0; i < Max_Nodes; i++)
-  {
-    readingHold2[i] = readingHold[m]; //readingHold2 now contains the path which is stored as bytes
-    Serial.print(readingHold[2]); Serial.print(" : ");
-    m += 1;
-  }
-  
-  //_convert_Str_to_IntArray(readingHold2);    //no idea why we cannot parse byte array
-  Serial.println("final path from casting: ");
-  for ( i = 0; i < sizeof(readingHold2); i++) {
-    if (isDigit(readingHold[i])) {
-      reading[i] = readingHold[i] - '0';
-      //reading[i] = byte(readingHold2[i]);
-      Serial.println(reading[i]);
-      //var=reading[i];
-      delay(100);
+  Serial.println(" ---------- element after 29th----------------------");
+
+    for (i = 0; i<sizeof(readingHold2); i++) {
+        readingHold2[i] = readingHold[m];
+        m++;
     }
+    
+  Serial.println(" \ndebugging reading path: readingHold2 ----------------------");
+  for (i = 0; i < sizeof(readingHold2); i++)
+  {
+    Serial.print(readingHold[2]); Serial.print(":");
   }
 
+    //pathToSend();         // No idea why this function doesn't work here.
 
   /* generic capture the right path, but contain some extra '1' from somewhere */
   //_convert_Str_to_IntArray();
 }
 
 
+/**
+ * this function converts path from bytes to int and then modifies the struct with path.//////////
+ */
+void pathToSend() {
+     //the loop below just converts the recieved from the api into a byte array which is then used to modify path in the struct to be transmitted. The string Read is just for debugging purposes
+  
+  int8_t b = 0;
+  int8_t a=0;
+  
+  for( i=0; i<sizeof(readingHold2); i++ )
+  {
+    
+    char var = readingHold2[i];
+    switch( var ) {
+      case 49:
+        readingHold3[a] = 1;
+        a++;
+        break;
+      case 50:
+        readingHold3[a] = 2;
+        a++;
+        break;
+      case 51:
+        readingHold3[a] = 3;
+        a++;
+        break;
+      case 52:
+        readingHold3[a] = 4;
+        a++;
+        break;
+      case 53:
+        readingHold3[a] = 5;
+        a++;
+        break;
+      case 54:
+        readingHold3[a] = 6;
+        a++;
+        break;
+      case 55:
+        readingHold3[a] = 7;
+        a++;
+        break;
+      case 56:
+        readingHold3[a] = 8;
+        a++;
+        break;
+      case 57:
+        readingHold3[a] = 9;
+        a++;
+        break;
+      case 48:
+        if( a < 1) {
+            readingHold3[a] = 0;
+            a++;
+            break;
+        }
+        else {}
+    }
+    /*
+    if(readingHold2[i] == 49)
+    {
+       readingHold3[a] = 1;
+       a+=1;
+    }
+    if(readingHold2[i] == 50)
+    {
+       readingHold3[a]=2;
+       a+=1;
+    }
+     if(readingHold2[i] == 51)
+    {
+       readingHold3[a] = 3;
+       a+=1;
+    }
+     if(readingHold2[i] == 52)
+    {
+       readingHold3[a] = 4;
+       a+=1;
+    }
+     if(readingHold2[i] == 53)
+    {
+       readingHold3[a]=5;
+       a+=1;
+    } 
+     if(readingHold2[i] == 48 && b<1)
+    {
+       readingHold3[a]=0;
+       a+=1;
+       b +=1;                           //ensures that 0 is written only ones in the path
+    }
+  } */
+  }
+  //b = 0;
+  //nodeID = String(readingHold3[4]);     //this holds the value of the requested node
+   //The data type path in my_Data below is modified so it contains the desired path to be sent
+  
+  for(i=0; i<sizeof(readingHold3); i++)  //The reason for using 5 is that the api test path is 3
+  {
+      My_Data.path[i] = readingHold3[i];
+  }
+}
+
+/*
 void _convert_Str_to_IntArray(byte path[]) {
   // works fine, but some extra '1' from somewhere
   Serial.println("Final path read from casting: ");
@@ -461,4 +541,4 @@ void _convert_Str_to_IntArray(byte path[]) {
     }
   }
 }
-
+*/
